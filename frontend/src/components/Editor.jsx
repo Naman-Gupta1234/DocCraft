37bee341aa33d9bf046956a4bf13cd5109f6ba8e
Naman Gupta <naman6112gupta@gmail.com>
2025-12-29 12:49:46 +0530

    Add frontend files properly

---
+++ frontend/src/components/Editor.jsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Quill from "quill";
+import { useParams } from "react-router-dom";
+// import 'quill/dist/quill.snow.css';
+import { io } from "socket.io-client";
+import styled from "@emotion/styled";
+
+import {MDBFooter} from 'mdb-react-ui-kit';
+const Component = styled.div`
+  background: #edf2fa;
+`;
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
+const Editor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const quillServer = new Quill("#container", {
+      theme: "snow",
+      modules: {
+        toolbar: toolbarOptions,
+      },
+    });
+    quillServer.disable();
+    quillServer.setText("Loading... the Document");
+    setQuill(quillServer);
+    // would work only when the component is initialized
+  }, []);
+
+  useEffect(() => {
+    const socketServer = io("http://localhost:9000");
+    setSocket(socketServer);
+    return () => {
+      socketServer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+
+    const handleChange = (delta, oldData, source) => {
+      if (source !== "user") return;
+      socket && socket.emit("send-changes", delta);
+    };
+    quill && quill.on("text-change", handleChange);
+    return () => {
+      quill && quill.off("text-change", handleChange);
+    };
+  }, [quill, socket]);
+
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+
+    const handleChange = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket && socket.on("receive-changes", handleChange);
+    return () => {
+      socket && socket.off("receive-changes", handleChange);
+    };
+  }, [quill, socket]);
+
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+    socket &&
+      socket.once("load-document", (document) => {
+        quill && quill.setContents(document);
+        quill && quill.enable();
+      });
+
+    socket && socket.emit("get-document", id);
+  }, [quill, socket, id]);
+
+  useEffect(() => {
+    if (socket === null || quill === null) return;
+    const interval = setInterval(() => {
+      socket && socket.emit("save-document", quill.getContents());
+    }, 2000);
+    return ()=>{
+      clearInterval(interval);
+    }
+  }, [socket, quill]);
+
+  return (
+    <Component>
+      <Box className="container" id="container"></Box>
+      <MDBFooter className='text-center text-white' style={{ backgroundColor: '#0a4275' }}>
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Made by -   Naman Gupta
+      </div>
+    </MDBFooter>
+    </Component>
+    // <Editor
+    // apiKey="Get your free API key at tiny.cloud and paste it here"
+    // plugins="wordcount"
+    //   />
+  );
+};
+
+export default Editor;
